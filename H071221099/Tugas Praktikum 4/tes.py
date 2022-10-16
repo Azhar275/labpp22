62f4fb404c22fdaf2ad6197dc51d0c29d73caabf
fathantsaqib <fathants12@gmail.com>
2022-10-16 21:14:24 +0800

    uploadtugas5

---
+++ H071221099/Tugas Praktikum 4/tes.py
@@ -0,0 +1,52 @@
+# list = [1, 2, 3, 3, 4, 6, 6, 10, 10]
+# for idx in range (len(list)):
+#     nilai1 = list [idx]
+#     for j in range (idx + 1, len(list)):
+#         nilai2 = list[j]
+#         if (nilai1 == nilai2):
+#             duplikat = nilai1
+#             print (duplikat)
+
+# list = [1, 2, 3, 3, 4, 5]
+# print(len(list))
+
+# for i in range (len(list)):
+#     print (list(i), end= " ")
+
+#dictionary
+# nama = input ("kasi masuk namata")
+# umur = int(input("kasi masuk umurta"))
+# hobi = input("kasi masuk hobita")
+
+# mydata = [
+# {
+#     "myname" : "fathan",
+#     "myage" : "18", 
+#     "myhobby" : "play"
+# }]
+
+# myfdata = [
+# {
+#     "myfname" : "jonathan",
+#     "myfage" : "17", 
+#     "myfhobby" : "coding"
+# }]
+
+# print ("data diri saya")
+# print (mydata[0])
+
+x = int (input("Input Array pertama: "))
+y = int (input("Input Array kedua: "))
+list = []
+for idx in range (len(list)):
+    nilai1 = list [idx]
+    for j in range (idx + 1, len(list)):
+        nilai2 = list[j]
+        if (nilai1 == nilai2):
+            duplikat = nilai1
+            print (duplikat)
+
+x = input ("angka: ")
+
+print (" |", x, x, "|", "x", "|", x, x, "|")
+print (" |", x, x, "|", " ", "|", x, x, "|")
