c24f67edf44a287269f8fb08f40e85010c5bab01
FailFudhayl <ahmadfudhayl06@gmail.com>
2022-10-29 14:01:06 +0800

    seventh commit

---
+++ H071221026/Tugas Praktikum 7/no1.py
@@ -0,0 +1,12 @@
+import re
+s= input()
+if len(s)==45 :
+    pola1= r"(\A([A-Za-z02468]){40})[\s13579]{5}"
+    result1= re.match(pola1, s)
+    if result1 :
+        print("true")
+    else :
+        print("false")
+else :
+    print("false")
+
