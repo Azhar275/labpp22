95bbad07aa8e8d73e7c3f93f7b6f753e5939145c
kelvinleonardos <kelvinlsianipar@gmail.com>
2022-11-12 13:26:57 +0800

    Tugas 8

--- H071221020/Tugas Praktikum 8/no1.py
+++ H071221020/Tugas Praktikum 8/no1.py
@@ -4,8 +4,6 @@ class Bio:
         self.name=name
         self.age=age
         self.isMale=isMale
-        if int(self.isMale)==0:
-            self.isMale=bool('')
 
     def setName(self, name):
         self.name=name
@@ -19,34 +17,26 @@ class Bio:
     def getAge(self):
         return self.age
 
-    def getisMale(self):
-        return self.isMale
-
-    def setGender(self, isMale):
+    def setGender(self):
         self.isMale=isMale
 
     def getGender(self):
-        if bio.getisMale()==True:
+        if self.isMale==True:
             return 'Laki-Laki'
         else:
             return 'Perempuan'
 
-while True:
-    name=input('name: ')
-    age=int(input('age: '))
-    isMale=bool(input('isMale: '))
+name=input('name: ')
+age=int(input('age: '))
+isMale=bool(input('isMale: '))
 
-    bio=Bio(name, age, isMale)
+bio=Bio(name, age, isMale)
 
-    bio.setName(bio.getName())
-    print(bio.getName())
+bio.setName(bio.getName())
+print(bio.getName())
 
-    bio.setAge(bio.getAge())
-    print(bio.getAge())
+bio.setAge(bio.getAge())
+print(bio.getAge())
 
-    bio.setGender(bio.getisMale())
-    print(bio.getGender())
-
-    x = int(input())
-    if x==0:
-        break
+bio.setGender()
+print(bio.getGender())
