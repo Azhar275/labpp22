af4961bc058530371593f7e929dbc8a1301de156
riooorante <rantetadungrio@gmail.com>
2022-11-24 13:37:52 +0800

    Tugas-2

--- H071221075/Tugas Praktikum 10/Nomor 1.py
+++ H071221075/Tugas Praktikum 10/Nomor 1.py
@@ -1,5 +1,5 @@
 import re
-from prettytable import PrettyTable
+# from prettytable import PrettyTable
 _Data = []
 while True:
     print(100*"="+"""\nPILIHAN LAYANAN
@@ -12,15 +12,15 @@ while True:
     _inputan = int(input(100*"="+"\nPilihan : ")); 
     if _inputan == 1: # Menampilkan Data Diri yang ada dalam list _Data
         if len(_Data) > 0: 
+            print(100*"=")
             for i in range(len(_Data)):
                 for y in range(len(_Data[0])):
                     print(f'{_Data[i][y]}')
-                    if y == 2:
-                        print("="*100)
         else:
             print(100*"="+"\nData saat ini kosong!\n"+100*"=")
     elif _inputan == 2: # Mengubah nama dalam list _Data
         if len(_Data) != 0:
+            print(100*"=")
             for i in range(len(_Data)):
                 for y in range(len(_Data[0])):
                     if y == 1 or y == 2: 
@@ -44,34 +44,31 @@ while True:
             print(100*"="+"\nData Sata Ini Kosong!\n"+100*"=")
         else:
             _FILE = input("Nama File : ")+".txt"
-            tabel = PrettyTable(["Data Tersimpan"])
-            tabel.align = "l"
             with open(_FILE, "a") as tulis:
+                tulis.write("\n")
                 for i in range(len(_Data)):
                     for y in range(len(_Data[0])):
-                        tabel.add_row([_Data[i][y]])
-                tulis.write(str(tabel))
+                        tulis.write(_Data[i][y]+"\n")
+                    tulis.write(100*"=")
                 _Data = []
     elif _inputan == 5: # Memasukkan data baru kedalam list _Data
         nama = input(100*"="+"\nNama : "); print(100*"=")
-        _cekEmail = "Not-Clear"
-        while _cekEmail == "Not-Clear":
+        while True:
             Email = input("Email : ")
-            if re.search(r"^[^A-Z_/-]{1,}@student.unhas.ac.id$", Email):
-                _cekEmail = "Clear"
+            if re.search(r"^[a-z0-9]{1,}@student[.]unhas[.]ac[.]id$", Email):
+                break
             else:
                 print(100*"="+"\nEmail Yang Anda Masukkan salah\n"+100*"=")
-        _cekPass = "Not-Clear"
-        while _cekPass == "Not-Clear":
+        while True:
             _Pass = input(100*"="+"\nMasukkan Password : "); print(100*"=")
             if len(_Pass) > 8:
                 if re.search('^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,20}$', _Pass):
-                    _cekPass = "Clear"
+                    break
                 else:
                     print("Password yang anda masukkan terlalu lemah, gunakan minimal 1 huruf kapital, huruf kecil, dan angka")
             else: 
                 print("Password Harus Memiliki 8-20 Karakter")
-            _Data.insert(len(_Data), ["Nama : " + nama,"E-mail : "+ Email, "Password : "+ _Pass])
+        _Data.insert(len(_Data), ["Nama : " + nama,"E-mail : "+ Email, "Password : "+ _Pass]); print("Berhasil")
     elif _inputan == 6: # Keluar dari program/menyelesaikan while
         print(100*"="+"\nSampai Jumpa Lagi\n"+100*"=")
         break
